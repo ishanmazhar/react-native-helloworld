675d959c0744344bab1d08f1fdedfbfcf5151a0f
ishanmazhar <ishanmazhar2@gmail.com>
2021-06-01 22:23:38 +0600

    Styling Basics

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const handleInputChange = text => {
+    alert(text)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+      {/* <Text>My First React Native App</Text>
+      <TextInput 
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        placeholder="I am a placeholder"
+        onChangeText={text => handleInputChange(text)}
+      />
+      <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -17,5 +28,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    // justifyContent: 'space-between',
+    // justifyContent: 'flex-start',
+    flexDirection: 'column'
+    // flexDirection: 'column-reverse'
+    // flexDirection: 'row'
+
   },
+  textStyle: {
+    color: "blue", 
+  }
 });
