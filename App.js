f8b296b112c441bf74aae3b8365da40213a510aa
ishanmazhar <ishanmazhar2@gmail.com>
2021-06-02 08:58:01 +0600

    Managing state with text input and button

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
-  const handleInputChange = text => {
-    alert(text)
-  }
+  const [inputValue, setInputValue] = useState(""); 
+  const [placeList, setPlaceList] = useState(""); 
   return (
     <View style={styles.container}>
+      <View style={styles.inputView}>
+        <TextInput 
+          style={{
+            width: "80%",
+            borderBottomWidth: 1,
+            borderColor: "green",
+            padding: 7
+          }}
+          placeholder="Add a Place.."
+          value={inputValue}
+          onChangeText={text => setInputValue(text)} 
+        />
+        <Button 
+          title="Add"
+          onPress={() => {
+            if(inputValue !== "") {
+              setPlaceList([...placeList, inputValue]);
+            }
+          }}
+        />
+      </View>
+      {/* <Text>Hello World!</Text>
       <Text>Hello World!</Text>
-      <Text>Hello World!</Text>
-      <Text>Hello World!</Text>
+      <Text>Hello World!</Text> */}
       {/* <Text>My First React Native App</Text>
       <TextInput 
         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
@@ -27,15 +47,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
     // justifyContent: 'space-between',
-    // justifyContent: 'flex-start',
+    justifyContent: 'flex-start',
     flexDirection: 'column'
     // flexDirection: 'column-reverse'
     // flexDirection: 'row'
 
   },
-  textStyle: {
-    color: "blue", 
+  inputView: {
+    padding: 20,
+    width: "100%", 
+    marginTop: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'center',
   }
 });
